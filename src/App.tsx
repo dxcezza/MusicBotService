ee6f2c694c8dd65904ffb8c9346cc6464b24d4cd
dxcezza <48048129+dxcezza@users.noreply.github.com>
2025-03-23 16:00:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -488,10 +488,10 @@ function App() {
                           value={band.gain}
                           onChange={(e) => handleEqualizerChange(index, parseFloat(e.target.value))}
                           className="vertical-slider h-48 w-2 bg-gray-600 rounded-lg appearance-none cursor-pointer
-                          [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4
-                          [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500"
+                                   [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4
+                                   [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500"
                           onTouchMove={(e) => e.stopPropagation()}
-                          />
+                        />
                         <span className="text-xs text-gray-400">{formatFrequency(band.frequency)}</span>
                       </div>
                     ))}
